3d3a9f9e6ca2119b7d17589ce769e543968adc5c
Rana-ElRobi <eng.rana.abdel-haleem@ieee.org>
2017-04-05 15:14:14 +0200

    tried another image viwing way where mouse hover it showes index of image , but i didnt work, imported liberary doesnt work

--- mainContour.py
+++ mainContour.py
@@ -2,6 +2,9 @@
 
 # import Needed packages
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from mpldatacursor import datacursor
 # Function that loads the target 10 images
 # 1st five are low degree tumor 
 # 2nd  five are hight degree tumor
@@ -21,12 +24,28 @@ def loadimages():
 		#cv2.imshow("colred img" , tempimg)
 		#cv2.imshow("grey img" , greytemp)
 		#cv2.waitKey(0)
+		# !!!! NOT working !!!
+		# Helper link
+		#  http://stackoverflow.com/questions/27704490/interactive-pixel-information-of-an-image-in-python
+		#fig, ax = plt.subplots()
+		#ax.imshow(tempimg, interpolation='none')
+		#datacursor(hover=True, bbox=dict(alpha=1, fc='w'))
+		#plt.show()
 	#return loaded imgs
 	return colorimgStack , greyimgStack
+# function that initialized the contour manually 
+def initContour():
+	# initialize empty list of lists
+	# each cell have list of 2 cells [x-coords ,y-coords]
+	icontour = []
+
+	# return initial contour
+	return icontour
 
 # main function for active contours
 def main():
 	# Load target images
 	colorimgStack , greyimgStack = loadimages()
-	
+	# initialize contour
+	baseContour = initContour()
 main() 
