cd2655fea8e1abca304dcde5b4e5659313280d4f
Rana-ElRobi <eng.rana.abdel-haleem@ieee.org>
2017-04-06 15:18:28 +0200

    deleted un nessesary function for image croping

--- mainContour.py
+++ mainContour.py
@@ -42,31 +42,6 @@ def initContour():
 
 	# return initial contour
 	return icontour
-# Trial function for selecting area then cropping
-def crop(img):
-	# initialize the list of reference points and boolean indicating
-	# whether cropping is being performed or not
-	refPt = []
-	cropping = False 
-	click_and_crop()
-def click_and_crop(event, x, y, flags, param):
-	# grab references to the global variables
-	global refPt, cropping
-	# if the left mouse button was clicked, record the starting
-	# (x, y) coordinates and indicate that cropping is being
-	# performed
-	if event == cv2.EVENT_LBUTTONDOWN:
-		refPt = [(x, y)]
-		cropping = True
-	# check to see if the left mouse button was released
-	elif event == cv2.EVENT_LBUTTONUP:
-		# record the ending (x, y) coordinates and indicate that
-		# the cropping operation is finished
-		refPt.append((x, y))
-		cropping = False
-		# draw a rectangle around the region of interest
-		cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
-		cv2.imshow("image", image)
 
 # main function for active contours
 def main():
